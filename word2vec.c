7d8cf0303333b5c04d4837e83e2875472cb50dc3
huajianjiu <lastranger8811@gmail.com>
2016-10-07 01:55:56 +0900

    a branch in which only remove praraphrases in negative sampling

--- word2vec.c
+++ word2vec.c
@@ -493,19 +493,12 @@ void *TrainModelThread(void *id) {
           for (c = 0; c < layer1_size; c++) syn1[c + l2] += g * neu1[c];
         }
         // NEGATIVE SAMPLING
-        // I modified this part to make paraphrases as positive samples
-        // and remove them in negative sampling. Yuanzhi Ke 2016
-        // For accuracy, now we only consider the most close paraphrase in ppdb_s, so just + 1        
-        if (negative > 0) for (d = 0; d < negative + 1 + 1; d++) {
+        // I modified this part to make paraphrases not
+        // removed in negative sampling. Yuanzhi Ke 2016
+        if (negative > 0) for (d = 0; d < negative + 1; d++) {
           if (d == 0) {
             target = word;
             label = 1;
-          } else if (d == 1) {
-            // 0(UNK) and -1 is not allowed. Yuanzhi Ke 2016
-            if (paraphrases[word] > 0) {
-              target = paraphrases[word];
-              label = 1;
-            } 
           } else {
             next_random = next_random * (unsigned long long)25214903917 + 11;
             target = table[(next_random >> 16) % table_size];
@@ -558,19 +551,12 @@ void *TrainModelThread(void *id) {
           for (c = 0; c < layer1_size; c++) syn1[c + l2] += g * syn0[c + l1];
         }
         // NEGATIVE SAMPLING
-        // I modified this part to make paraphrases as positive samples
-        // and remove them in negative sampling. Yuanzhi Ke 2016
-        // For accuracy, now we only consider the most close paraphrase in ppdb_s, so just + 1
-        if (negative > 0) for (d = 0; d < negative + 1 +1 ; d++) {
+        // I modified this part to make paraphrases not
+        // removed in negative sampling. Yuanzhi Ke 2016
+        if (negative > 0) for (d = 0; d < negative + 1 ; d++) {
           if (d == 0) {
             target = word;
             label = 1;
-          } else if (d == 1) {
-            // 0(UNK) and -1 is not allowed. Yuanzhi Ke 2016
-            if (paraphrases[word] > 0) {
-              target = paraphrases[word];
-              label = 1;
-            } 
           } else {
             next_random = next_random * (unsigned long long)25214903917 + 11;
             target = table[(next_random >> 16) % table_size];
