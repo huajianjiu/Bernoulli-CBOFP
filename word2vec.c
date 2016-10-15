f35ef405d298b2ee2426c8b0b5d80b2aec930330
huajianjiu <lastranger8811@gmail.com>
2016-10-16 02:18:39 +0900

    fix error

--- word2vec.c
+++ word2vec.c
@@ -424,7 +424,7 @@ void InitNet() {
 }
 
 void *TrainModelThread(void *id) {
-  long long a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
+  long long a, b, d, cw, word, last_word, wi, sentence_length = 0, sentence_position = 0;
   long long word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
   long long l1, l2, c, target, label, local_iter = iter;
   unsigned long long next_random = (long long)id;
