46e3dba1d6342857f5ff167f8a5c4cbada25a96d
huajianjiu <lastranger8811@gmail.com>
2017-01-05 15:18:25 +0900

    update

--- tuning-size-experiment_t9_f1.sh
+++ tuning-size-experiment_t9_f1.sh
@@ -10,5 +10,6 @@ do
 	time ./fuzzypp_word2vec -train text9 -output "${VECTORDIR}"/f1_t9_s${i}w8n25 -cbow 1 -size ${i} -window 8 -negative 25 -sample 1e-4 -threads 20 -iter 15 -dropout 0;
 	./compute-accuracy "${VECTORDIR}"/f1_t9_s${i}w8n25.bin < questions-words.txt > "${RESULTDIR}"/qw_f1_t9_s${i}w8n25.txt;
 	python compute-wordsim.py "${VECTORDIR}"/f1_t9_s${i}w8n25.bin.txt WS353.csv > "${RESULTDIR}"/353_f1_t9_s${i}w8n25.txt;
-	python compute-wordsim.py "${VECTORDIR}"/f1_t9_s${i}w8n25.bin.txt SimLex999.csv > "${RESULTDIR}"/999_f1_t9_s${i}w8n25.txt;
+	python compute-wordsim.py "${VECTORDIR}"/f1_t9_s${i}w8n25.bin.txt SimLex-999.csv > "${RESULTDIR}"/999_f1_t9_s${i}w8n25.txt;
+	python compute-wordsim.py "${VECTORDIR}"/f1_t9_s${i}w8n25.bin.txt rw.csv > "${RESULTDIR}"/rw_f1_t9_s${i}w8n25.txt;
 done
